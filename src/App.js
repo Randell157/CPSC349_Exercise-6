d2e2e654987ab9ff1ea49457183118bebae8aa2f
Randell Lapid <randelljustinlapid@gmail.com>
2023-11-24 23:44:40 -0800

    fixed game

--- src/App.js
+++ src/App.js
@@ -54,12 +54,12 @@ function Board({ xIsNext, squares, onPlay }) {
 
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
-    setXIsNext(xIsNext);
+    setXIsNext(!xIsNext);
   }
 
   return (
